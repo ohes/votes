8ea79be979d1172710ebd930da793e0fa78d7e38
Esther Oh <estheroh@gmail.com>
2015-08-02 01:25:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Congress::Application.config.secret_key_base = '40ba0b6d8ece3f6604149f0d8b8845f5a0c4ca950d79fd566c073b60fb79786a47f4b744fffa03689577682caac3b7621a31f885c6557924ac7a602b84b553d7'
